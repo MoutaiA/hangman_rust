34db54acfa8bd71f45e98fef4c92a09c2e6d680d
amoutai <amin.moutai@newmips.com>
2023-03-27 11:15:54 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{io};
+
 fn main() {
-    println!("Hello, world!");
+    let mystery_word = String::from("toto");
+    display_mystery_word(mystery_word);
+    println!("{}", get_user_input());
+}
+
+fn display_mystery_word(mystery_word: String) {
+    println!("The mystery word is {}", mystery_word)
+}
+
+fn get_user_input() -> String {
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+		Ok(_) => {}
+		Err(error) => println!("{}", error)
+	}
+
+    return input;
 }
