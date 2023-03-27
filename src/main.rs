941eca3f497ef92feaf023e461747c48d52907e9
amoutai <amin.moutai@newmips.com>
2023-03-27 16:14:57 +0200

    adding display text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::*;
 use std::collections::HashSet;
 use std::io;
 
@@ -5,48 +6,107 @@ const END_GAME: u8 = 0;
 const STOP_GAME: &str = "STOP";
 
 fn main() {
+	//TODO: API
     let mystery_word = String::from("toto");
     let mut counter = mystery_word.len();
     let mut characters: HashSet<String> = HashSet::new();
+    let mut response = set_response(&mystery_word, &characters);
+
+    display_welcome();
+    display_response(&response);
 
     while counter > END_GAME.into() {
-        let user_input: String = get_user_input();
+        let mut user_input: String = get_user_input();
+        user_input = String::from(user_input.trim());
 
-        if characters.contains(&user_input) {
-            let values: Vec<String> = get_all_characters(&characters);
-            print!("Please, enter a character which hasn't been used yeat, here the characters already set: {:?}", values);
-            println!();
-            continue;
+        if STOP_GAME == user_input {
+            break;
         }
 
-        if STOP_GAME == user_input.trim() {
-            println!("Bye!");
-            break;
+        if characters.contains(&user_input) {
+            display_character_already_set(&characters);
+            continue;
         }
 
         characters.insert(user_input.clone());
 
-        if is_same_word(&mystery_word, &user_input) {
-            println!("GG");
-            break;
-        } else if is_part(&mystery_word, &user_input) {
-            //TODO:
+        if is_part(&mystery_word, &user_input) {
+            response = set_response(&mystery_word, &characters);
+            if is_response_mystery_word(&mystery_word, &response) {
+                println!("You did it!");
+                break;
+            }
         } else {
             counter -= 1;
         }
+        display_response(&response);
     }
+    println!("Bye!");
+}
+
+fn display_welcome() {
+    println!();
+    println!(
+        "{}",
+        "Welcome to the hangman"
+            .yellow()
+            .bold()
+            .reverse()
+            .underline()
+    );
+    println!("{}", "Please enter only one character at the time, if you enter multiple, only the first one will be read.".bold());
+    println!(
+        "{} {}",
+        "Also, if you want to quit, just enter ".bold(),
+        STOP_GAME.red().bold()
+    );
+    println!(
+        "{}",
+        "Finally, only word between a to z, and on lowercase are allowed".bold()
+    );
+    println!();
+}
+
+fn display_response(response: &str) {
+    println!("Your current response is: {}", response);
+}
+
+fn set_response(mystery_word: &str, characters: &HashSet<String>) -> String {
+    let mut response = String::with_capacity(mystery_word.len() * 2 - 1);
+
+    for c in mystery_word.chars() {
+        if characters.contains(&c.to_string()) {
+            response += &c.to_string();
+        } else {
+            response += "_ ";
+        }
+    }
+
+    return response;
 }
 
 fn get_user_input() -> String {
     let mut input = String::new();
 
-    println!("Please, enter a character, or 'STOP', if you want to stop the game");
+    println!("{}", "Next character?".bold());
+	//TODO: handle none
     match io::stdin().read_line(&mut input) {
-        Ok(_) => {}
+        Ok(_) => {
+            let trimmed_input = String::from(input.trim());
+            if trimmed_input != STOP_GAME {
+                input = trimmed_input.chars().nth(0).unwrap().to_string();
+            }
+        }
         Err(error) => println!("{}", error),
     }
 
     return input;
+}
+
+fn display_character_already_set(characters: &HashSet<String>) {
+    let values: Vec<String> = get_all_characters(&characters);
+    print!("Please, enter a character which hasn't been used yeat, here the characters already set: {:?}", values);
+    println!();
 }
 
 fn get_all_characters(characters: &HashSet<String>) -> Vec<String> {
@@ -59,10 +119,11 @@ fn get_all_characters(characters: &HashSet<String>) -> Vec<String> {
     return arr;
 }
 
-fn is_same_word(mystery_word: &str, user_input: &str) -> bool {
-    return mystery_word.trim() == user_input.trim();
-}
-
+//TODO:
 fn is_part(mystery_word: &str, user_input: &str) -> bool {
     return true;
+}
+
+fn is_response_mystery_word(mystery_word: &str, response: &str) -> bool {
+    return mystery_word == response;
 }
